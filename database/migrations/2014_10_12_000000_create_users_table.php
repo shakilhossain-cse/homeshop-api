a66377188fc89794aab3c1f03a938d52c36ad9b8
shakilhossain <shakilhossainp.cse@gmail.com>
2023-04-16 16:18:51 +0600

    add cors middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (!Schema::hasTable('users')) {
+     if (!Schema::hasTable('users')) {
             Schema::create('users', function (Blueprint $table) {
                 $table->bigIncrements('id');
                 $table->string('first_name');
